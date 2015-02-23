27c01178ef231a8625580eb35c35387575384663
Yuriy Bodnar <yabodnar@gmail.com>
2015-02-22 20:12:27 -0500

    * Replaced prints with logging * get_cluster now accepts path to cluster.json

--- hdpaws/ambari.py
+++ hdpaws/ambari.py
@@ -1,7 +1,15 @@
 __author__ = 'yuriybodnar'
 
 import requests, json
+import logging
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 class AmbariService():
     def __init__(self, url, config):
@@ -16,8 +24,8 @@ class AmbariService():
         return ''.join(res)
 
 
-    def get_cluster(self, hostname):
-        file = open(self.config.ambari_cluster_file, "r")
+    def get_cluster_definition(self, hostname, cluster_file=None):
+        file = open(self.config.ambari_cluster_file if cluster_file is None else cluster_file, "r")
         res = file.readlines()
         return ''.join(res).replace("PARAM_HOST_NAME", hostname)
 
@@ -26,28 +34,40 @@ class AmbariService():
         if blueprint is None:
             blueprint = self.get_blueprint()
         service = "api/v1/blueprints/{blueprint_name}".format(blueprint_name=blueprint_name)
+        logger.info("Sending blueprint")
+        logger.debug(blueprint)
         response = requests.post('/'.join([self.url, service]),
                                  json=blueprint, auth=self.credentials,
                                  headers=self.headers)
         status = response.status_code
         if status != 201:
-            print "[register_blueprint] Error:", response.text
+            logger.error(status)
+            logger.error(response.text)
+        else:
+            logger.info("Registered blueprint successfully")
 
     def register_cluster(self, cluster, cluster_name="TestCluster"):
         service = "api/v1/clusters/{cluster_name}".format(cluster_name=cluster_name)
+        logger.info("Registering cluster")
+        logger.debug("Sending cluster configuration")
+        logger.debug(cluster)
         response = requests.post('/'.join([self.url, service]),
                                  json=cluster, auth=self.credentials,
                                  headers=self.headers)
         status = response.status_code
         if status != 201:
-            print "[register_cluster] Error:", response.text
+            logger.error(response.status_code)
+            logger.error(response.text)
+        else:
+            logger.info("Successfully registered cluster")
 
     def start_cluster(self, hostname):
-        cluster_def = self.get_cluster(hostname)
+        logger.info("Starting cluster")
+        cluster_def = self.get_cluster_definition(hostname)
         self.register_cluster(cluster_def)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     import configyb
-    ambari = AmbariService("http://ec2-54-152-83-80.compute-1.amazonaws.com:8080", configyb)
-    # ambari.register_blueprint()
-    ambari.register_cluster(ambari.get_cluster("ip-172-30-0-124.ec2.internal"))
+    # ambari = AmbariService("http://ec2-54-152-83-80.compute-1.amazonaws.com:8080", configyb)
+    # # ambari.register_blueprint()
+    # ambari.register_cluster(ambari.get_cluster("ip-172-30-0-124.ec2.internal"))
