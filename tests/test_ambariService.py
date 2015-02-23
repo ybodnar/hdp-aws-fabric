ea6bfef6dc4c14c6d45a472773acc7b0d8ec8661
Yuriy Bodnar <yabodnar@gmail.com>
2015-02-22 20:12:47 -0500

    Disabled (almost)tests

---
+++ tests/test_ambariService.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+__author__ = 'yuriybodnar'
+
+
+class TestAmbariService(TestCase):
+    def test_get_cluster_definition(self):
+        pass
+        # from hdpaws import AmbariService
+        # import configyb
+        #
+        # service = AmbariService("bla-bla", configyb)
+        # print service.get_cluster_definition("my.example.com", "../cluster/cluster.json")
