ea9a0c42234726790c5f5800248b0d327b67ca89
Yuriy Bodnar <yabodnar@gmail.com>
2015-02-22 20:13:26 -0500

    *Replaced prints with loggings *Pushed through config

--- hdpaws/util.py
+++ hdpaws/util.py
@@ -1,11 +1,14 @@
 __author__ = 'yuriybodnar'
 
+import logging
+
+
 def get_ambari_url(instance):
     return "".join(["http://", instance.public_dns_name, ":8080"])
 
 
-def report(instance):
-    print "Instance ID:", instance.id
-    print "Instance public DNS", instance.public_dns_name
-    print "To connect via SSH:  ", "ssh", "@".join([config.aws_ami_default_user, instance.public_dns_name])
-    print "Ambari:  ", get_ambari_url(instance)
+def report(instance, config):
+    logging.info("Instance ID: %s" % instance.id)
+    logging.info("Instance public DNS: %s" % instance.public_dns_name)
+    logging.info("To connect via SSH: %s" % "ssh", "@".join([config.aws_ami_default_user, instance.public_dns_name]))
+    logging.info("Ambari: %s" % get_ambari_url(instance))
